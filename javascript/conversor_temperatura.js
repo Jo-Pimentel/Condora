c071ba26765d01f32d264c375a0e10e03b8bccc5
Jo-Pimentel <joaopodolsky59@gmail.com>
2025-10-17 20:11:07 -0300

    Conversor de temperatura completamente funcional, mas que terá adições, futuramente.

--- javascript/conversor_temperatura.js
+++ javascript/conversor_temperatura.js
@@ -29,31 +29,35 @@ escala_conversao.addEventListener("change", () => {
 btn_converter_temperatura.addEventListener("click", () => {
     let temperatura_convertida;
 
-    if(escala_atual.value == escala_conversao.value) {
-        alert("É impossível converter uma temperatura para a escala que ela já está.");
-    } else if(!temperatura_inicial.value) {
-        alert("Defina um valor para a temperatura que será convertida.");
-    } else if(temperatura_inicial.value < zero_absoluto) {
-        alert(`Temperatura inválida para ${escala_atual.value}, pois ultrapassa o valor de ${zero_absoluto}.`);
-    } else {
-        if(escala_atual.value == "Celsius") {
-            if(escala_conversao.value == "Farenheit") {
-                temperatura_convertida = ((parseInt(temperatura_inicial.value) * 9) + (5 * 32)) / 5;
-            } else {
-                temperatura_convertida = parseInt(temperatura_inicial.value) + 273;
-            }
-        } else if(escala_atual.value == "Farenheit") {
-            if(escala_conversao.value == "Celsius") {
-                temperatura_convertida = ((parseInt(temperatura_inicial.value) * 5) - (32 * 5)) / 9;
-            } else {
-                temperatura_convertida = (((parseInt(temperatura_inicial.value) * 5) - (32 * 5)) + (273 * 9)) / 9;
-            }
+    if(escala_atual.value == "Celsius") {
+        if(escala_conversao.value == "Farenheit") {
+            temperatura_convertida = ((parseInt(temperatura_inicial.value) * 9) + (5 * 32)) / 5;
         } else {
-            if(escala_conversao.value == "Celsius") {
-                temperatura_convertida = parseInt(temperatura_inicial.value) - 273;
-            } else {
-                temperatura_convertida = (((parseInt(temperatura_inicial.value) * 9) - (273 * 9)) + (32 * 5)) / 5;
-            }
+            temperatura_convertida = parseInt(temperatura_inicial.value) + 273;
         }
+    } else if(escala_atual.value == "Farenheit") {
+        if(escala_conversao.value == "Celsius") {
+            temperatura_convertida = ((parseInt(temperatura_inicial.value) * 5) - (32 * 5)) / 9;
+        } else {
+            temperatura_convertida = (((parseInt(temperatura_inicial.value) * 5) - (32 * 5)) + (273 * 9)) / 9;
+        }
+    } else {
+        if(escala_conversao.value == "Celsius") {
+            temperatura_convertida = parseInt(temperatura_inicial.value) - 273;
+        } else {
+            temperatura_convertida = (((parseInt(temperatura_inicial.value) * 9) - (273 * 9)) + (32 * 5)) / 5;
+        }
+    }
+
+    document.querySelector("#resposta_temperatura_convertida").textContent = temperatura_convertida;
+
+    try {
+        if(escala_atual.value == escala_conversao.value) throw "É impossível converter uma temperatura para a escala que ela já está.";
+
+        if(!temperatura_inicial.value) throw "Defina um valor para a temperatura pré conversão.";
+
+        if(temperatura_inicial.value < zero_absoluto) throw `Temperatura inválida para ${escala_atual.value}, pois ultrapassa o valor de ${zero_absoluto}`;
+    } catch(erro) {
+        document.querySelector("#resposta_temperatura_convertida").textContent = erro;
     }
 })
